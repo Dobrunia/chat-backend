42724ffc9d4871a97e3dc6ffc42fe09289f317f9
Dobrunia <dobkos@hotmail.com>
2023-04-07 00:33:28 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3333;
-
+import * as http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
+app.use(express.json());
+const server = http.createServer(app);
+const db = { messages: [] as string[] };
+const io = new Server(server, {
+  cors: {
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+  },
+}); //in case server and client run on different urls
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+io.on('connection', (socket) => {
+  console.log('client connected: ', socket.id);
+  socket.join('clock-room');
+  socket.on('disconnect', (reason) => {
+    console.log(reason);
+  });
 });
-
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+server.listen(PORT, () => {
+  console.log('Server running on Port ', PORT);
+});
+// setInterval(() => {
+//   io.to('clock-room').emit('hehe', new Date());
+// }, 1000);
+app.post('/messages', (req, res) => {
+  console.log(req);
+  const { message } = req.body;
+  db.messages.push(message);
+  io.to('clock-room').emit('message', message);
+  // res.json({ data: new Date() });
 });
